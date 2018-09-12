67b423d4444ec4c0cbd7e667e1f368a1203a3629
Almog Laktivi <al942u@att.com>
2018-09-12 17:49:23 +0300

    Add module-2

--- basics/module-2/starter/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('app listening on port 3000!'));
-//# sourceMappingURL=app.js.map
