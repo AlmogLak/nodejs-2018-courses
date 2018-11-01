30177c4cdda8956fb72035ca53a52bd11116d574
Almog Laktivi <al942u@att.com>
2018-11-01 17:49:42 +0200

    Change exc of module 3

--- frameworks/module-3/solution/persistance/fs.persistanse.ts
+++ frameworks/module-3/solution/persistance/fs.persistanse.ts
@@ -48,7 +48,6 @@ export class FSPersistance implements PersistanceInterface {
                 if (err) {
                     reject(err);
                 }
-
                 resolve(item);
             });
         });
