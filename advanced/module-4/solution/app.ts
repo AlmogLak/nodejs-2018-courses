8771638e35ac4b2a87bac445e870d933b118de7b
Almog Laktivi <al942u@att.com>
2018-11-20 18:47:00 +0200

    Add module 4 for the advanced course

--- advanced/module-4/solution/app.ts
+++ advanced/module-4/solution/app.ts
@@ -9,7 +9,7 @@ server.on("connection", socket => {
 
     socket.on("data", data => {
         console.log(`host recived from client: ${data.toString()}`);
-        dns.resolve(data.toString(), "MX", (err, addresses) => {
+        dns.resolve4(data.toString(), (err, addresses) => {
             if (err) {
                 socket.write(`error occurred: ${err}\n\n`);
                 return;
