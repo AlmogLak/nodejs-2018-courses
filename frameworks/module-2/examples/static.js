75281b20dca3fed369be1f8c0c63fdafeac1491f
Almog Laktivi <al942u@att.com>
2020-01-05 23:49:53 +0200

    Add static file serve to express module

---
+++ frameworks/module-2/examples/static.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.use('/static', express.static('../../assets'));
+app.use(express.static('../../assets'));
+// http://localhost:3000/att_logo.png
+// http://localhost:3000/static/att_logo.png
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+//# sourceMappingURL=static.js.map
---
+++ frameworks/module-2/examples/static.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+const app = express()
+
+app.use('/static', express.static('../../assets'));
+app.use(express.static('../../assets'));
+// http://localhost:3000/att_logo.png
+// http://localhost:3000/static/att_logo.png
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
